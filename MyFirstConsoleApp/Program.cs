199e1624b8452fb380f9c5312e7e6ce6cb1b7f34
Nathan Gonzalez <ncgonz@gmail.com>
2021-07-17 11:23:39 -0500

    added some more examples

--- MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/Program.cs
@@ -13,13 +13,27 @@ namespace MyFirstConsoleApp
             Console.WriteLine("Please enter your name...");
             name = Console.ReadLine();
 
+            //console.log(`my name is ${name}`);
+
+
+            //Combining string
+
+            //string concatenation
+            Console.WriteLine("Hello, " + name);
+
+            //string formatting
+            Console.WriteLine(string.Format("Hello, {0}, {1}, {2}", name));
+
+            //string interpolation
+            Console.WriteLine($"Hello, {name}");
+
             //this will not work because static types
             //name = 7;
 
             //csharp has a lot of specificty around numbers
             //32 bit signed number
             int favoriteNumber = -6;
-            
+
             //64 bit signed numbers
             long reallyBigNumber = 9_223_372_036_854_775_806;
 
@@ -40,7 +54,7 @@ namespace MyFirstConsoleApp
             var isTeaching = true;
 
             //datetime is for holding.... datetime
-            var today = new DateTime(2021,7,13);
+            var today = new DateTime(2021, 7, 13);
             today = DateTime.Today;
 
 
@@ -57,19 +71,46 @@ namespace MyFirstConsoleApp
 
             students.Remove("Jimbo");
 
-            for(var i = 0; i < students.Count; i++)
+            for (var i = 0; i < students.Count; i++)
             {
                 Console.WriteLine(students[i]);
             }
 
             //more idiomatic
-            foreach(var student in students)
+            foreach (var student in students)
             {
                 Console.WriteLine(student);
             }
 
             //csharp one equal for set and two for equality
+            if (name == "Nathan")
+            {
+                Console.WriteLine("Hey it me");
+            }
+            else
+            {
+                Console.WriteLine("Not me");
+            }
 
+            //ternary (not technically control flow)
+            var greeting = name == "Nathan" ? "It is you." : "It is not you";
+
+
+            //anonymous types
+            var nathan = new { Name = "Nathan", IsTeacher = true };
+
+            //doesn't work, anonymous types don't allow for modification (immutable)
+            //nathan.Name = "Steve";
+
+
+            //objects break the rules sometimes because of being a reference type
+            object animalType = "Dog";
+            animalType = 45;
+
+            //boxing moves values to the heap
+            if (animalType.ToString() == "Dog")
+            {
+            }
         }
     }
 }
